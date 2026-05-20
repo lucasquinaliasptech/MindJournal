0a0af16e8f9557b48223b3eb68b3b2d96037cadd
lucasquinaliasptech <lucas.quinalia@sptech.school>
2026-05-20 20:05:54 -0300

    Implementação da exibição do dashboard vazio e do botão de logou

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -3,8 +3,9 @@ function validarSessao() {
     var id = sessionStorage.ID_USUARIO;
     var pessoa = document.getElementById("icone-pessoa");
     var dashboard = document.getElementById("icone-dashboard");
+    var logout = document.getElementById("logout");
 
-    id != null ? (pessoa.href = "perfil.html", dashboard.href = "dashboard.html") : (pessoa.href = "cadastro.html", dashboard.href = "cadastro.html");
+    id != null ? (pessoa.href = "perfil.html", dashboard.href = "dashboard.html", logout.style.display = "flex") : (pessoa.href = "cadastro.html", dashboard.href = "cadastro.html", logout.style.display = "none");
 }
 
 function limparSessao() {
